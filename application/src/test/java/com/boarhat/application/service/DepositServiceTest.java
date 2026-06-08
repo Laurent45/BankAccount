be758cc475f43992e60f8005d115bdebb49eaa55
Laurent FREDERICK <laurent.frederick@icloud.com>
2026-06-08 11:39:38 -0400

    Refactor deposit service test

--- application/src/test/java/com/boarhat/application/service/DepositServiceTest.java
+++ application/src/test/java/com/boarhat/application/service/DepositServiceTest.java
@@ -5,6 +5,7 @@ import com.boarhat.application.exception.AccountNotFoundException;
 import com.boarhat.application.fake.InMemoryAccountRepository;
 import com.boarhat.domain.account.AccountId;
 import com.boarhat.domain.account.BankAccount;
+import com.boarhat.domain.account.OverdraftAuthorization;
 import com.boarhat.domain.shared.Amount;
 import com.boarhat.domain.shared.Balance;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -34,15 +36,16 @@ class DepositServiceTest {
 
         @BeforeEach
         void givenAccountExists() {
-            accountRepository.save(BankAccount.create(ACCOUNT_ID));
+            accountRepository.save(BankAccount.reconstruct(
+                    ACCOUNT_ID, Balance.of(new BigDecimal("100")), OverdraftAuthorization.notAllowed(), List.of()));
         }
 
         @Test
-        void should_increase_balance_after_deposit() {
-            depositService.deposit(new DepositCommand(ACCOUNT_ID, Amount.of(new BigDecimal("200"))));
+        void should_increase_balance_by_deposited_amount() {
+            depositService.deposit(new DepositCommand(ACCOUNT_ID, Amount.of(new BigDecimal("50"))));
 
-            Balance balance = accountRepository.getOrThrow(ACCOUNT_ID).getBalance();
-            assertThat(balance).isEqualTo(Balance.of(new BigDecimal("200")));
+            assertThat(accountRepository.getOrThrow(ACCOUNT_ID).getBalance())
+                    .isEqualTo(Balance.of(new BigDecimal("150")));
         }
     }
 
